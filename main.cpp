67c954aa961f24a87ca2f3b0e34575f06ed5774a
Simon Chen <ychen442@my.bcit.ca>
2018-09-14 11:32:04 -0700

    changing printout

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include "gcd.hpp"
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    using namespace std;
+
+    cout<<gcd(12,24)<<endl;
+
     return 0;
+}
+
+// Greatest Common Denominator
+// PRE : a is a positive integer
+// PRE : b is a positive integer
+// POST : a and b are unchanged
+// RETURN : the greatest common denominator of
+// a and b.
+int gcd ( const int a, const int b){
+    if(a == 0 || b == 0){
+        return 0;
+    }
+
+    if( a > b){
+        for(int i = b; i > 1; i--){
+            if(b % i == 0 && a % i == 0){
+                return i;
+            }
+        }
+    } else if( b >= a){
+        for(int i = a; i > 1; i--){
+            if(b % i == 0 && a % i == 0){
+                return i;
+            }
+        }
+    }
+
+    return 1;
 }
